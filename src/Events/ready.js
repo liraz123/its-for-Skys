a0670d3bf69ea28ccbd9f00b8dd67c3656480cdf
liraz123 <89916408+liraz123@users.noreply.github.com>
2021-10-17 14:43:36 +0300

    Create ready.js

---
+++ src/Events/ready.js
@@ -0,0 +1,14 @@
+/** @format */
+
+const Event = require("../Structures/Event.js");
+
+module.exports = new Event("ready", (client) => {
+console.log("bot is online!")
+        setInterval(()=>{
+
+ const status = activites[Math.floor(Math.random()*activites.length)]
+ client.user.setPresence
+ ({activities : [{name: `${status}`}]}) },4000)
+                                          
+        
+        const activites = ['GAME 1', 'GAME 2']});
